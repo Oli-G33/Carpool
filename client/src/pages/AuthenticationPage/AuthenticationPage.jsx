33d9f375db65cf19228556a6cebd8ca6cf0dd35a
Oliver Garcia <96548026+Oli-G33@users.noreply.github.com>
2023-06-04 05:40:26 +0000

    Centered AuthenticationPage

--- client/src/pages/AuthenticationPage/AuthenticationPage.jsx
+++ client/src/pages/AuthenticationPage/AuthenticationPage.jsx
@@ -1,19 +1,29 @@
 import React, { useState } from 'react';
 import Login from '../../components/login';
 import Register from '../../components/register';
-import { Box } from '@mui/material';
+import { Box, Container } from '@mui/material';
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
 
   return (
-    <Box flexGrow={0}>
-      {isLogin ? (
-        <Login isLogin={isLogin} setIsLogin={setIsLogin} />
-      ) : (
-        <Register isLogin={isLogin} setIsLogin={setIsLogin} />
-      )}
-    </Box>
+    <Container
+      maxWidth="xs"
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        minHeight: '100vh',
+      }}
+    >
+      <Box flexGrow={0}>
+        {isLogin ? (
+          <Login isLogin={isLogin} setIsLogin={setIsLogin} />
+        ) : (
+          <Register isLogin={isLogin} setIsLogin={setIsLogin} />
+        )}
+      </Box>
+    </Container>
   );
 };
 
