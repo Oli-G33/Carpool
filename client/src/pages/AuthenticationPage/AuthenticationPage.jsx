0895e6fa763b87a2e571c8d52598c2cf1cc4a0f3
OliGarc333 <ngarcia333@gmail.com>
2023-07-02 08:03:28 +0200

    Added API call to start Render server asap

--- client/src/pages/AuthenticationPage/AuthenticationPage.jsx
+++ client/src/pages/AuthenticationPage/AuthenticationPage.jsx
@@ -2,9 +2,21 @@ import React, { useState } from 'react';
 import Login from '../../components/login';
 import Register from '../../components/register';
 import { Box, Container } from '@mui/material';
+import { wakeApi } from '../../services/api';
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
+
+  useEffect(() => {
+    // Starts backend server on pageload
+    wakeApi()
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
 
   return (
     <Container
--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,4 +4,6 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_REST_API_URL
 });
 
+export const wakeApi = api.get('/');
+
 export default api;
