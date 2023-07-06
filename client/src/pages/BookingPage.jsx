d7f6928fe941885af0d83dadbfbfb240a28e25d0
Oliver Garcia <96548026+Oli-G33@users.noreply.github.com>
2023-07-06 09:09:31 +0000

    Changed booking button

--- client/src/pages/BookingPage.jsx
+++ client/src/pages/BookingPage.jsx
@@ -228,7 +228,7 @@ const BookingPage = () => {
             disabled={availableSeats <= 0 || isLoading || !!alertMessage}
             startIcon={isLoading ? <CircularProgress size={24} /> : null}
           >
-            Confirm
+           Book
           </Button>
         </Box>
       </Container>
