3f877185560f78e5fd0acc102c3caebc54e8fc34
OliGarc333 <ngarcia333@gmail.com>
2023-09-03 13:05:56 +0200

    Styled successpage

--- client/src/pages/SuccessPage.jsx
+++ client/src/pages/SuccessPage.jsx
@@ -23,43 +23,55 @@ const SuccessPage = () => {
         justifyContent="center"
       >
         <Container maxWidth="sm">
-          <Box textAlign="center" py={10}>
-            <Typography
-              variant="h4"
-              component="h1"
-              gutterBottom
-              sx={{
-                color: 'white'
-              }}
-            >
-              Ride Booked Successfully!
-            </Typography>
-            <Box display="flex" justifyContent="center">
-              <ConfettiExplosion
-                particleCount={300}
-                duration={5000}
-                force={1}
-                width={1500}
-              />
-            </Box>
-            <Typography
-              variant="body1"
-              sx={{
-                color: 'white'
-              }}
-            >
-              Thank you for booking your ride. We look forward to seeing you on
-              the selected date.
-            </Typography>
-            <Box mt={4}>
-              <Button
-                component={Link}
-                to="/booking"
-                variant="contained"
-                color="primary"
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'flex-start',
+              backgroundColor: 'rgba(200, 200, 200, 0.6)',
+              border: '1px solid #ccc',
+              borderRadius: '8px'
+            }}
+          >
+            <Box textAlign="center" py={10}>
+              <Typography
+                variant="h4"
+                component="h1"
+                gutterBottom
+                sx={{
+                  color: 'white'
+                }}
               >
-                Book More Rides
-              </Button>
+                Ride Booked Successfully!
+              </Typography>
+              <Box display="flex" justifyContent="center">
+                <ConfettiExplosion
+                  particleCount={300}
+                  duration={5000}
+                  force={1}
+                  width={1500}
+                />
+              </Box>
+
+              <Typography
+                variant="body1"
+                sx={{
+                  color: 'white'
+                }}
+              >
+                Your ride has been requested! The driver will confirm the ride
+                details by email as soon as possible.
+              </Typography>
+              <Box mt={4}>
+                <Button
+                  component={Link}
+                  to="/booking"
+                  variant="contained"
+                  color="primary"
+                >
+                  Request More Rides
+                </Button>
+              </Box>
             </Box>
           </Box>
         </Container>
