20637a2ddbfe44d1a6ae69d04de5df3e0d8f4a62
Oliver Garcia <96548026+Oli-G33@users.noreply.github.com>
2023-09-26 06:41:07 +0000

    Change to Success page

--- client/src/pages/SuccessPage.jsx
+++ client/src/pages/SuccessPage.jsx
@@ -62,6 +62,14 @@ const SuccessPage = () => {
                 Your ride has been requested! The driver will confirm the ride
                 details by email as soon as possible.
               </Typography>
+              <Typography
+                variant="body1"
+                sx={{
+                  color: 'white'
+                }}
+              >
+               !!!Please be at your meeting point 3 minutes before the designated pickup time!!!
+              </Typography>
               <Box mt={4}>
                 <Button
                   component={Link}
